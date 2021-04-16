8cdce064a89bd6a1b72ac8942610a721e265f46d
test-coin-trx <75595280+test-coin-trx@users.noreply.github.com>
2021-04-17 00:54:50 +0200

    revert

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,10 +2,10 @@ import { Colors } from "./types";
 
 export const baseColors = {
   failure: "#ED4B9E",
-  primary: "#FF8640",
-  primaryBright: "#FF8640",
-  primaryDark: "#FF8640",
-  secondary: "#2E6301",
+  primary: "#aa8929",
+  primaryBright: "#aa8929",
+  primaryDark: "#aa8929",
+  secondary: "#7645D9",
   success: "#31D0AA",
   warning: "#FFB237",
 };
@@ -23,9 +23,9 @@ export const lightColors: Colors = {
   invertedContrast: "#FFFFFF",
   input: "#eeeaf4",
   tertiary: "#EFF4F5",
-  text: "#2E6301",
+  text: "#452A7A",
   textDisabled: "#BDC2C4",
-  textSubtle: "#428C03",
+  textSubtle: "#8f80ba",
   borderColor: "#E9EAEB",
   card: "#FFFFFF",
   gradients: {
@@ -44,9 +44,9 @@ export const darkColors: Colors = {
   input: "#483f5a",
   primaryDark: "#0098A1",
   tertiary: "#353547",
-  text: "#DCFAC3",
+  text: "#EAE2FC",
   textDisabled: "#666171",
-  textSubtle: "#d9f5c1",
+  textSubtle: "#c9c4d4",
   borderColor: "#524B63",
   card: "#27262c",
   gradients: {
