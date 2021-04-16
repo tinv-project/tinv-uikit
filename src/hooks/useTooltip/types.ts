8b03bda6171c4f236d81c8a4b1a1e7518147baea
test-coin-trx <75595280+test-coin-trx@users.noreply.github.com>
2021-04-17 00:25:58 +0200

    delete

--- src/hooks/useTooltip/types.ts
+++
@@ -1,7 +0,0 @@
-export interface TooltipRefs {
-  targetRef: React.Dispatch<React.SetStateAction<HTMLElement | null>>;
-  tooltip: React.ReactNode;
-  tooltipVisible: boolean;
-}
-
-export type TriggerType = "click" | "hover" | "focus";
